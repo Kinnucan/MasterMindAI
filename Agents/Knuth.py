1e96fb47cbb27f1983ec329657d8910cac252469
Kinnucan <kinnucanr@gmail.com>
2020-11-29 10:04:46 -0600

    Working on Knuth

--- Agents/Knuth.py
+++ Agents/Knuth.py
@@ -8,9 +8,11 @@ from itertools import permutations
 class Knuth:
 
     def __init__(self, gameManager):
+        self.Set = self.generateSet()
         Agent.__init__(self, gameManager, verbose=True, seeCode=False)
 
-    def createGuess(self):
+    def createGuess(self, blackPegs, whitePegs):
+        return 0
 
     def generateSet(self):
         colors = ["red", "orange", "yellow", "green", "blue", "purple"]
@@ -28,8 +30,28 @@ class Knuth:
                         setOfCodes.append(permutation)
         return setOfCodes
 
+    def miniMax(self):
+        return 0
+
     def play(self):
         initialGuess = Code([CodePin("red"), CodePin("red"), CodePin("orange"), CodePin("orange")])
         initialResponse = self.environment.guessCode(initialGuess)
 
-        # ADD IF STATEMENT CHECKING IF RESPONSE IS FOUR COLORED PEGS
+        blackPegs = initialResponse.getBlackPegs()
+        whitePegs = initialResponse.getWhitePegs()
+
+        if (initialResponse.isWinning()):
+            print("Won in", self.environment.getGuessNumber(), "guesses!")
+        else:
+            while True:
+                newGuess = self.createGuess(blackPegs, whitePegs)
+                newResponse = self.environment.guessCode(newGuess)
+                if self.verbose:
+                    print("Guess:", newGuess, "Response:", newResponse)
+                if newResponse.isWinning():
+                    if self.verbose:
+                        print("Won in", self.environment.getGuessNumber(), "guesses!")
+                    break
+                blackPegs = newResponse.getBlackPegs()
+                whitePegs = newResponse.getWhitePegs()
+            return self.environment.getGuessNumber()
