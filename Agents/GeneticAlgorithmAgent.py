d561b38c0ea3256eeb4f2e73c7ab2dcb9c996e05
Arthur DressenWall <arthurdanieldw@gmail.com>
2020-11-28 21:02:56 -0600

    More work on the genetic algorithm

--- Agents/GeneticAlgorithmAgent.py
+++ Agents/GeneticAlgorithmAgent.py
@@ -3,7 +3,8 @@ from Agent import *
 class GeneticAlgorithmAgent(Agent):
     #implements the genetic algorithm proposed by Berghman, Goossens, and Leus
 
-    def __init__(self, gameManager, verbose=False, seeCode=False, firstGuess = makeCode([1]*PIN_NUMBER), popSize = 150, maxGen = 100, maxSize = 60):
+    def __init__(self, gameManager, verbose=False, seeCode=False, firstGuess = makeCode([1]*PIN_NUMBER), popSize = 150, maxGen = 100, maxSize = 60,
+                 onePointCrossoverProb = .5, colorChangeProb = .03, permutationProb = .03, inversionProb = .02, a = 1, b = 2, EHatChoiceMethod = "min"):
         Agent.__init__(self, gameManager, verbose, seeCode)
         self.firstGuess = firstGuess # the paper recommends (1,1,2,3) -- "red red orange yellow" -- as an ideal first guess
         self.popSize = popSize # the size of the initial population
@@ -11,18 +12,46 @@ class GeneticAlgorithmAgent(Agent):
         self.maxSize = maxSize # how many eligible codes we consider after each use of the genetic algorithm
 
         self.guessNumber = 1
-        self.currentGen = 1
-        self.EHat = [] # the set of eligible combinations created
 
 
     def createGuess(self):
         if self.guessNumber == 1:
             return self.firstGuess
-        self.currentGen = 1
-        self.EHat = []
+        currentGen = 1
+        EHat = []  # the set of eligible combinations created
         population = self.initializePopulation()
+        while currentGen <= self.maxGen and len(EHat) <= self.maxSize:
+            population = self.makeNewPopulation(population)
+            for code in population:
+                if self.isConsistent(code):
+                    EHat.append(code)
+            currentGen += 1
+        return self.chooseFromEHat(EHat)
+
 
 
     def initializePopulation(self):
-        return []
+        output = []
+        for i in range(self.popSize):
+            newCode = Code([])
+            while newCode in output:
+                newCode.randomize()
+            output.append(newCode)
+        return output
 
+    def makeNewPopulation(self, pop):
+        output = self.crossover(pop)
+        output = self.mutate(output)
+        return output
+
+    def crossover(self, pop):
+        return pop
+
+    def mutate(self, pop):
+        return pop
+
+    def fitness(self, code):
+        return 1
+
+    def chooseFromEHat(self, possibleCodes):
+        return None
