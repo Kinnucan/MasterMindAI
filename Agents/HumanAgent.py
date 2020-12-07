20751d0ad3844db97da27aa09427d08484e1ea6f
lprehn <lprehn@macalester.edu>
2020-12-06 22:46:46 -0600

    Tested progress in HumanAgent

--- Agents/HumanAgent.py
+++ Agents/HumanAgent.py
@@ -15,7 +15,7 @@ def randomColor(viableColors):
 # choose two different colors randomly from a given list
 def randomTwoColors(viableColors):
     if len(viableColors) > 2:
-        clist = viableColors
+        clist = viableColors.copy()
         color1 = randomColor(clist)
         clist.remove(color1)
         color2 = randomColor(clist)
@@ -42,14 +42,17 @@ class HumanAgent(Agent):
         self.guessList = self.environment.getGuessList()
         self.clueList = self.environment.getClueList()
         # a temporary list used to narrow down color choices for a single guess
-        clist = self.viableColors
+        clist = self.viableColors.copy()
         # analyze the most recent clue, if there is one (empty clues count)
         if self.guessCount > 0:
             result = self.analyzeLatestClue()
         # first phase: keep guessing until you receive at least one clue in return
+        print("testRound is: " + str(self.testRound))
         if self.testRound == 0:
-            color1, color2 = randomTwoColors(self.viableColors)
-            self.guess = Code([CodePin(color1), CodePin(color1), CodePin(color2), CodePin(color2)])
+            color1, color2 = randomTwoColors(clist)
+            clist.remove(color1)
+            clist.remove(color2)
+            newGuess = Code([CodePin(color1), CodePin(color1), CodePin(color2), CodePin(color2)])
         # second phase: switch out one color to see how it affects the given clue
         else:
             if self.testRound == 1:
@@ -63,17 +66,22 @@ class HumanAgent(Agent):
                 newColor = randomColor(clist)
                 # choose one of the previous colors to reuse, and replace the other color with the new selection
                 if random.randint(0, 1) == 0:
-                    self.guess = Code([CodePin(prevColor1), CodePin(prevColor1), CodePin(newColor), CodePin(newColor)])
+                    newGuess = Code([CodePin(prevColor1), CodePin(prevColor1), CodePin(newColor), CodePin(newColor)])
                 else:
-                    self.guess = Code([CodePin(newColor), CodePin(newColor), CodePin(prevColor2), CodePin(prevColor2)])
+                    newGuess = Code([CodePin(newColor), CodePin(newColor), CodePin(prevColor2), CodePin(prevColor2)])
             else:
                 if self.testRound == 2:
                     # retrieve the clue given after the last round
                     lastClue = self.clueList[self.guessCount]
-                    # TODO: Before continuing, test this out
+        # update the guess list
+        self.guessList.append(newGuess)
+        self.guess = newGuess
+        self.guessCount += 1
+        print("The agent's guess is: " + self.guess.__str__())
 
     def analyzeLatestClue(self):
-        clue = self.guessList[self.guessCount-1]
+        clue = self.clueList[self.guessCount-1]
+
         # check to see if the game has been won; if so, end the method
         if clue.getBlackPegs() == 4:
             self.won = True
@@ -95,4 +103,15 @@ class HumanAgent(Agent):
                 else:
                     return False
 
+    def go(self):
+        self.environment.generateCode()
+        for i in range(2):
+            self.makeGuess()
+        print("done!")
 
+    def makeGuess(self):
+        self.createGuess()
+        clue = self.environment.guessCode(self.guess)
+        self.clueList.append(clue)
+        print("The clue list was " + clue.__str__())
+        print("The true code was " + self.environment.getCode().__str__())
